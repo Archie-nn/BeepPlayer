06da43610894dcc3ae5a1dfcc33e9e73f0b85684
Archie_nn <yangenqi805@gmail.com>
2026-05-09 21:28:58 +0800

    加入專案檔案。

---
+++ Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace 簡易電子琴
+{
+    partial class frmBeepPlayer
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.palMain = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.palMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // palMain
+            // 
+            this.palMain.Controls.Add(this.button8);
+            this.palMain.Controls.Add(this.button7);
+            this.palMain.Controls.Add(this.button6);
+            this.palMain.Controls.Add(this.button5);
+            this.palMain.Controls.Add(this.button4);
+            this.palMain.Controls.Add(this.button3);
+            this.palMain.Controls.Add(this.button2);
+            this.palMain.Controls.Add(this.button1);
+            this.palMain.Location = new System.Drawing.Point(30, 24);
+            this.palMain.Name = "palMain";
+            this.palMain.Size = new System.Drawing.Size(636, 150);
+            this.palMain.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 18);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(69, 111);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Do";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(90, 18);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(69, 111);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Re";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(165, 18);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(69, 111);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Mi";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(240, 18);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(69, 111);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Fa";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(315, 18);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(69, 111);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "So";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(390, 18);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(69, 111);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "La";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(465, 18);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(69, 111);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "Si";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button8.Location = new System.Drawing.Point(540, 18);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(69, 111);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "Do";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // frmBeepPlayer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(709, 209);
+            this.Controls.Add(this.palMain);
+            this.Name = "frmBeepPlayer";
+            this.Text = "簡易電子琴";
+            this.Load += new System.EventHandler(this.frmBeepPlayer_Load);
+            this.SizeChanged += new System.EventHandler(this.frmBeepPlayer_SizeChanged);
+            this.palMain.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel palMain;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button8;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 簡易電子琴
+{
+    public partial class frmBeepPlayer: Form
+    {
+        public frmBeepPlayer()
+        {
+            InitializeComponent();
+            InitializeButton(); // 必須呼叫此行，琴鍵點擊才會生效
+        }
+        [DllImport("kernel32.dll")]
+        public static extern bool Beep(int frequency, int duration);
+        int[] freq = { 523, 587, 659, 698, 784, 880, 988, 1046 };
+
+        int initWidth = 0;
+        int initHeight = 0;
+        // Define the Rect structure
+        public struct Rect
+        {
+            public int Left;
+            public int Top;
+            public int Width;
+            public int Height;
+
+            public Rect(int left, int top, int width, int height)
+            {
+                Left = left;
+                Top = top;
+                Width = width;
+                Height = height;
+            }
+        }
+        Dictionary<string, Rect> initControl = new Dictionary<string, Rect>();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            btn.Enabled = false;
+            Beep(freq[btn.TabIndex], 300);
+            btn.Enabled = true;
+        }
+        private void InitializeButton()
+        {
+            // 讓btn1~btn8共用同一個事件處理函式
+            button2.Click += button1_Click;
+            button3.Click += button1_Click;
+            button4.Click += button1_Click;
+            button5.Click += button1_Click;
+            button6.Click += button1_Click;
+            button7.Click += button1_Click;
+            button8.Click += button1_Click;
+        }
+
+        private void frmBeepPlayer_Load(object sender, EventArgs e)
+        {
+            this.initWidth = this.palMain.Width;
+            this.initHeight = this.palMain.Height;
+            foreach (Control ctl in this.palMain.Controls)
+            {
+                this.initControl.Add(ctl.Name, new Rect(ctl.Left, ctl.Top,
+                ctl.Width, ctl.Height));
+            }
+        }
+
+        private void frmBeepPlayer_SizeChanged(object sender, EventArgs e)
+        {
+            // 1.防呆：如果字典還沒初始化（是空的），就先跳過，避免啟動時崩潰
+            if (initControl == null || initControl.Count == 0) return;
+            double width = this.palMain.Width;
+            double height = this.palMain.Height;
+            // 2. 防呆：避免除以零（當視窗縮到最小時）
+            if (this.initWidth == 0 || this.initHeight == 0) return;
+            double iRatioWith = width / this.initWidth;
+            double iRatioHeight = height / this.initHeight;
+            foreach (Control ctl in this.palMain.Controls)
+            {
+                // 3. 安全檢查：確保字典裡真的有這個控制項的原始資料
+                if (initControl.ContainsKey(ctl.Name))
+                {
+                    var originalRect = initControl[ctl.Name];
+                    ctl.Left = (int)(originalRect.Left * iRatioWith);
+                    ctl.Top = (int)(originalRect.Top * iRatioHeight);
+                    ctl.Width = (int)(originalRect.Width * iRatioWith);
+                    ctl.Height = (int)(originalRect.Height * iRatioHeight);
+                }
+
+            }
+        }
+    }
+}
